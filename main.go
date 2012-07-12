67e8819d1e0de805e6cf0fd73be398a0b7a30bd7
Peter Bourgon <peter@bourgon.org>
2012-07-12 20:50:30 +0200

    Handle _static

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"github.com/peterbourgon/bonus/xlog"
+	"strings"
 )
 
 var (
@@ -30,23 +32,38 @@ func init() {
 //  - somehow defaults for unspecified variables
 
 func main() {
+	if err := os.MkdirAll(*outputDir, 0755); err != nil {
+		xlog.Fatalf("%s: %s", *outputDir, err)
+	}
+
+	// copy static
+	copyStatic := exec.Command("cp", "-r", *staticDir+"/", *outputDir+"/")
+	buf, err := copyStatic.CombinedOutput()
+	if err != nil {
+		xlog.Fatalf("copying %s: %s (%s)", *staticDir, err, strings.TrimSpace(string(buf)))
+	}
+
+	// render source files
 	sourceFiles, err := filepath.Glob(*sourceDir + "/*")
 	if err != nil {
 		xlog.Fatalf("%s: %s", *sourceDir, err)
 	}
 	for _, sourceFile := range sourceFiles {
+		// extract context
 		templateFile, ctx, outputFile, err := ContextFrom(sourceFile)
 		if err != nil {
 			xlog.Problemf("Context: %s: %s", sourceFile, err)
 			continue
 		}
 
+		// render using specified template
 		buf, err := RenderTemplate(*templateDir, templateFile, ctx)
 		if err != nil {
 			xlog.Problemf("Render: %s: %s", sourceFile, err)
 			continue
 		}
 
+		// write output
 		totalOutputFile := *outputDir + "/" + outputFile
 		if err := os.MkdirAll(filepath.Dir(totalOutputFile), 0755); err != nil {
 			xlog.Problemf("MkdirAll: %s: %s: %s", sourceFile, outputFile, err)
@@ -58,7 +75,6 @@ func main() {
 			continue
 		}
 		defer f.Close()
-
 		n, err := f.Write(buf)
 		if err != nil {
 			xlog.Problemf("Write: %s: %s: %s", sourceFile, outputFile, err)
