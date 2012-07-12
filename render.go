8210c1e63d03fbf04c724b24d19edf41d5d15fdc
Peter Bourgon <peter@bourgon.org>
2012-07-12 21:08:02 +0200

    Better blackfriday options

--- render.go
+++ render.go
@@ -19,7 +19,19 @@ func RenderTemplate(templateDir, inFile string, ctx Context) ([]byte, error) {
 	// Second, inject MarkdownKey into the ctx, from BodyKey content (if any)
 	if i, ok := ctx[BodyKey]; ok {
 		if s, ok := i.(string); ok {
-			ctx[MarkdownKey] = string(blackfriday.MarkdownBasic([]byte(s)))
+			htmlOptions := blackfriday.HTML_GITHUB_BLOCKCODE | blackfriday.HTML_USE_SMARTYPANTS
+			htmlRenderer := blackfriday.HtmlRenderer(
+				htmlOptions,
+				"", // title
+				"", // css
+			)
+			markdownOptions := blackfriday.EXTENSION_FENCED_CODE
+			buf := blackfriday.Markdown(
+				[]byte(s),
+				htmlRenderer,
+				markdownOptions,
+			)
+			ctx[MarkdownKey] = string(buf)
 		}
 	}
 
